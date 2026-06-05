055722bd1db2faffa96e9b4ef68556fde184dc1f
Arjan <Arjan.van.Vught@gmail.com>
2026-06-05 19:57:03 +0200

    Add lib-superloop and integrate into build
    Introduce a new lib-superloop library (Eclipse project files, Makefiles, Rules.mk, and settings) and add it to the firmware build list. Move superloop software timer sources/headers from lib-hal to lib-superloop, update file headers (copyright year), rename the timer callback parameter in the header, and fix a stray character in the error message. Update GD32 Includes.mk to prefer FreeRTOS includes when USE_FREE_RTOS is set and otherwise add lib-superloop include path. Also add small formatting/namespace cleanup in lib-flashcode/include/flashcode.h and add lib-superloop to firmware-template/libs.mk.

--- firmware-template/libs.mk
+++ firmware-template/libs.mk
@@ -29,6 +29,6 @@ ifeq ($(findstring DISPLAY_UDF,$(DEFINES)),DISPLAY_UDF)
 	LIBS+=displayudf
 endif
 
-LIBS+=configstore flashcode display hal hwclock
+LIBS+=configstore flashcode display superloop hal hwclock
 
 $(info $$LIBS [${LIBS}])
