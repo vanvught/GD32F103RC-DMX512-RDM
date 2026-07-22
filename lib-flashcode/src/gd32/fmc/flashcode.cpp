1a8e2e56abf48ef8639552fc47dff8cd42c51e4c
Arjan <Arjan.van.Vught@gmail.com>
2026-07-22 22:28:46 +0200

    Refactor GD32 flashcode state and formatting
    Clean up both GD32 flashcode implementations with consistent brace/style formatting and tighter symbol scoping. In `h7xx/flashcode.cpp`, move constants/state globals into an anonymous namespace and rename `State::IDLE` to `State::kIdle` for clearer enum style while keeping behavior unchanged.

--- lib-flashcode/src/gd32/fmc/flashcode.cpp
+++ lib-flashcode/src/gd32/fmc/flashcode.cpp
@@ -34,17 +34,15 @@
  * With the latest GD32F firmware, this function is declared as static.
  */
 #if defined(GD32F20X)
-extern "C"
-{
-    fmc_state_enum fmc_bank0_state_get(void);
-    fmc_state_enum fmc_bank1_state_get(void);
+extern "C" {
+fmc_state_enum fmc_bank0_state_get(void);
+fmc_state_enum fmc_bank1_state_get(void);
 }
 #endif
 
 #include "firmware/debug/debug_debug.h"
 
-namespace flashcode
-{
+namespace flashcode {
 /* Backwards compatibility with SPI FLASH */
 static constexpr auto kFlashSectorSize = 4096U;
 /* The flash page size is 2KB for bank0 */
@@ -52,15 +50,7 @@ static constexpr auto kBanK0FlashPage = (2U * 1024U);
 /* The flash page size is 4KB for bank1 */
 static constexpr auto kBanK1FlashPage = (4U * 1024U);
 
-enum class State
-{
-    IDLE,
-    ERASE_BUSY,
-    ERASE_PROGAM,
-    WRITE_BUSY,
-    WRITE_PROGRAM,
-    ERROR
-};
+enum class State { IDLE, ERASE_BUSY, ERASE_PROGAM, WRITE_BUSY, WRITE_PROGRAM, ERROR };
 
 static State s_state = State::IDLE;
 static uint32_t s_page;
@@ -70,17 +60,12 @@ static uint32_t* s_data;
 static bool s_isBank0;
 } // namespace flashcode
 
-bool static is_bank0(const uint32_t page_address)
-{
+bool static is_bank0(const uint32_t page_address) {
     /* flash size is greater than 512k */
-    if (FMC_BANK0_SIZE < FMC_SIZE)
-    {
-        if (FMC_BANK0_END_ADDRESS > page_address)
-        {
+    if (FMC_BANK0_SIZE < FMC_SIZE) {
+        if (FMC_BANK0_END_ADDRESS > page_address) {
             return true;
-        }
-        else
-        {
+        } else {
             return false;
         }
     }
@@ -90,26 +75,22 @@ bool static is_bank0(const uint32_t page_address)
 
 using namespace flashcode;
 
-uint32_t FlashCode::GetSize() const
-{
+uint32_t FlashCode::GetSize() const {
     return FMC_SIZE * 1024U;
 }
 
-uint32_t FlashCode::GetSectorSize() const
-{
+uint32_t FlashCode::GetSectorSize() const {
     return kFlashSectorSize;
 }
 
-bool FlashCode::Read(uint32_t offset, uint32_t length, uint8_t* pBuffer, flashcode::Result& result)
-{
+bool FlashCode::Read(uint32_t offset, uint32_t length, uint8_t* pBuffer, flashcode::Result& result) {
     DEBUG_ENTRY();
     DEBUG_PRINTF("offset=%p[%d], len=%u[%d], data=%p[%d]", offset, (((uint32_t)(offset) & 0x3) == 0), length, (((uint32_t)(length) & 0x3) == 0), pBuffer, (((uint32_t)(pBuffer) & 0x3) == 0));
 
     const auto* pSrc = reinterpret_cast<uint32_t*>(offset + FLASH_BASE);
     auto* pDst = reinterpret_cast<uint32_t*>(pBuffer);
 
-    while (length > 0)
-    {
+    while (length > 0) {
         *pDst++ = *pSrc++;
         length -= 4;
     }
@@ -120,24 +101,19 @@ bool FlashCode::Read(uint32_t offset, uint32_t length, uint8_t* pBuffer, flashco
     return true;
 }
 
-bool FlashCode::Erase(uint32_t offset, uint32_t length, flashcode::Result& result)
-{
+bool FlashCode::Erase(uint32_t offset, uint32_t length, flashcode::Result& result) {
     DEBUG_ENTRY();
     DEBUG_PRINTF("State=%d", static_cast<int>(s_state));
 
     result = Result::kOk;
 
-    switch (s_state)
-    {
+    switch (s_state) {
         case State::IDLE:
             s_page = offset + FLASH_BASE;
             s_length = length;
-            if ((s_isBank0 = is_bank0(s_page)))
-            {
+            if ((s_isBank0 = is_bank0(s_page))) {
                 fmc_bank0_unlock();
-            }
-            else
-            {
+            } else {
                 fmc_bank1_unlock();
             }
             s_state = State::ERASE_BUSY;
@@ -146,40 +122,28 @@ bool FlashCode::Erase(uint32_t offset, uint32_t length, flashcode::Result& resul
             return false;
             break;
         case State::ERASE_BUSY:
-            if (s_isBank0)
-            {
-                if (FMC_BUSY == fmc_bank0_state_get())
-                {
+            if (s_isBank0) {
+                if (FMC_BUSY == fmc_bank0_state_get()) {
                     DEBUG_EXIT();
                     return false;
                 }
-            }
-            else
-            {
-                if (FMC_BUSY == fmc_bank1_state_get())
-                {
+            } else {
+                if (FMC_BUSY == fmc_bank1_state_get()) {
                     DEBUG_EXIT();
                     return false;
                 }
             }
 
-            if (s_isBank0)
-            {
+            if (s_isBank0) {
                 FMC_CTL0 &= ~FMC_CTL0_PER;
-            }
-            else
-            {
+            } else {
                 FMC_CTL1 &= ~FMC_CTL1_PER;
             }
 
-            if (s_length == 0)
-            {
-                if (s_isBank0)
-                {
+            if (s_length == 0) {
+                if (s_isBank0) {
                     fmc_bank0_lock();
-                }
-                else
-                {
+                } else {
                     fmc_bank1_lock();
                 }
                 s_state = State::IDLE;
@@ -192,25 +156,20 @@ bool FlashCode::Erase(uint32_t offset, uint32_t length, flashcode::Result& resul
             return false;
             break;
         case State::ERASE_PROGAM:
-            if (s_length > 0)
-            {
+            if (s_length > 0) {
                 DEBUG_PRINTF("s_page=%p", s_page);
 
-                if (s_isBank0)
-                {
+                if (s_isBank0) {
                     FMC_CTL0 |= FMC_CTL0_PER;
                     FMC_ADDR0 = s_page;
                     FMC_CTL0 |= FMC_CTL0_START;
 
                     s_length -= kBanK0FlashPage;
                     s_page += kBanK0FlashPage;
-                }
-                else
-                {
+                } else {
                     FMC_CTL1 |= FMC_CTL1_PER;
                     FMC_ADDR1 = s_page;
-                    if (FMC_OBSTAT & FMC_OBSTAT_SPC)
-                    {
+                    if (FMC_OBSTAT & FMC_OBSTAT_SPC) {
                         FMC_ADDR0 = s_page;
                     }
                     FMC_CTL1 |= FMC_CTL1_START;
@@ -225,12 +184,9 @@ bool FlashCode::Erase(uint32_t offset, uint32_t length, flashcode::Result& resul
             return false;
             break;
         case State::WRITE_BUSY:
-            if (s_isBank0)
-            {
+            if (s_isBank0) {
                 FMC_CTL0 &= ~FMC_CTL0_PG;
-            }
-            else
-            {
+            } else {
                 FMC_CTL1 &= ~FMC_CTL1_PG;
             }
             /*@fallthrough@*/
@@ -251,23 +207,18 @@ bool FlashCode::Erase(uint32_t offset, uint32_t length, flashcode::Result& resul
     return true;
 }
 
-bool FlashCode::Write(uint32_t offset, uint32_t length, const uint8_t* pBuffer, flashcode::Result& result)
-{
+bool FlashCode::Write(uint32_t offset, uint32_t length, const uint8_t* pBuffer, flashcode::Result& result) {
     result = Result::kOk;
 
-    switch (s_state)
-    {
+    switch (s_state) {
         case State::IDLE:
             DEBUG_PUTS("State::IDLE");
             s_address = offset + FLASH_BASE;
             s_data = const_cast<uint32_t*>(reinterpret_cast<const uint32_t*>(pBuffer));
             s_length = length;
-            if ((s_isBank0 = is_bank0(s_address)))
-            {
+            if ((s_isBank0 = is_bank0(s_address))) {
                 fmc_bank0_unlock();
-            }
-            else
-            {
+            } else {
                 fmc_bank1_unlock();
             }
             s_state = State::WRITE_BUSY;
@@ -276,40 +227,28 @@ bool FlashCode::Write(uint32_t offset, uint32_t length, const uint8_t* pBuffer, 
             return false;
             break;
         case State::WRITE_BUSY:
-            if (s_isBank0)
-            {
-                if (FMC_BUSY == fmc_bank0_state_get())
-                {
+            if (s_isBank0) {
+                if (FMC_BUSY == fmc_bank0_state_get()) {
                     DEBUG_EXIT();
                     return false;
                 }
-            }
-            else
-            {
-                if (FMC_BUSY == fmc_bank1_state_get())
-                {
+            } else {
+                if (FMC_BUSY == fmc_bank1_state_get()) {
                     DEBUG_EXIT();
                     return false;
                 }
             }
 
-            if (s_isBank0)
-            {
+            if (s_isBank0) {
                 FMC_CTL0 &= ~FMC_CTL0_PG;
-            }
-            else
-            {
+            } else {
                 FMC_CTL1 &= ~FMC_CTL1_PG;
             }
 
-            if (s_length == 0)
-            {
-                if (s_isBank0)
-                {
+            if (s_length == 0) {
+                if (s_isBank0) {
                     fmc_bank0_lock();
-                }
-                else
-                {
+                } else {
                     fmc_bank1_lock();
                 }
                 s_state = State::IDLE;
@@ -321,14 +260,10 @@ bool FlashCode::Write(uint32_t offset, uint32_t length, const uint8_t* pBuffer, 
             return false;
             break;
         case State::WRITE_PROGRAM:
-            if (s_length >= 4)
-            {
-                if (s_isBank0)
-                {
+            if (s_length >= 4) {
+                if (s_isBank0) {
                     FMC_CTL0 |= FMC_CTL0_PG;
-                }
-                else
-                {
+                } else {
                     FMC_CTL1 |= FMC_CTL1_PG;
                 }
                 REG32(s_address) = *s_data;
@@ -336,15 +271,10 @@ bool FlashCode::Write(uint32_t offset, uint32_t length, const uint8_t* pBuffer, 
                 s_data++;
                 s_address += 4;
                 s_length -= 4;
-            }
-            else if (s_length > 0)
-            {
-                if (s_isBank0)
-                {
+            } else if (s_length > 0) {
+                if (s_isBank0) {
                     FMC_CTL0 |= FMC_CTL0_PG;
-                }
-                else
-                {
+                } else {
                     FMC_CTL1 |= FMC_CTL1_PG;
                 }
                 REG32(s_address) = *s_data;
@@ -353,12 +283,9 @@ bool FlashCode::Write(uint32_t offset, uint32_t length, const uint8_t* pBuffer, 
             return false;
             break;
         case State::ERASE_BUSY:
-            if (s_isBank0)
-            {
+            if (s_isBank0) {
                 FMC_CTL0 &= ~FMC_CTL0_PER;
-            }
-            else
-            {
+            } else {
                 FMC_CTL1 &= ~FMC_CTL1_PER;
             }
             /*@fallthrough@*/
--- lib-flashcode/src/gd32/h7xx/flashcode.cpp
+++ lib-flashcode/src/gd32/h7xx/flashcode.cpp
@@ -32,50 +32,40 @@
 #include "gd32.h"
 #include "firmware/debug/debug_debug.h"
 
-/* Backwards compatibility with SPI FLASH */
-static constexpr auto kFlashSectorSize = 4096U;
-/* The flash page size is 4KB for bank1 */
-static constexpr auto kBanK1FlashPage = (4U * 1024U);
+namespace {
+// Backwards compatibility with SPI FLASH
+constexpr auto kFlashSectorSize = 4096U;
+// The flash page size is 4KB for bank1
+constexpr auto kBanK1FlashPage = (4U * 1024U);
 
-enum class State
-{
-    IDLE,
-    ERASE_BUSY,
-    ERASE_PROGAM,
-    WRITE_BUSY,
-    WRITE_PROGRAM,
-    ERROR
-};
+enum class State { kIdle, ERASE_BUSY, ERASE_PROGAM, WRITE_BUSY, WRITE_PROGRAM, ERROR };
 
-static State s_state = State::IDLE;
-static uint32_t s_page;
-static uint32_t s_length;
-static uint32_t s_address;
-static uint32_t* s_data;
+State s_state = State::kIdle;
+uint32_t s_page;
+uint32_t s_length;
+uint32_t s_address;
+uint32_t* s_data;
+} // namespace
 
 using flashcode::Result;
 
-uint32_t FlashCode::GetSize() const
-{
+uint32_t FlashCode::GetSize() const {
     const auto kFlashDensity = ((REG32(0x1FF0F7E0) >> 16) & 0xFFFF) * 1024U;
     return kFlashDensity;
 }
 
-uint32_t FlashCode::GetSectorSize() const
-{
+uint32_t FlashCode::GetSectorSize() const {
     return kFlashSectorSize;
 }
 
-bool FlashCode::Read(uint32_t offset, uint32_t length, uint8_t* buffer, Result& result)
-{
+bool FlashCode::Read(uint32_t offset, uint32_t length, uint8_t* buffer, Result& result) {
     DEBUG_ENTRY();
     DEBUG_PRINTF("offset=%p[%d], len=%u[%d], data=%p[%d]", offset, (((uint32_t)(offset) & 0x3) == 0), length, (((uint32_t)(length) & 0x3) == 0), buffer, (((uint32_t)(buffer) & 0x3) == 0));
 
     const auto* src = reinterpret_cast<uint32_t*>(offset + FLASH_BASE);
     auto* dst = reinterpret_cast<uint32_t*>(buffer);
 
-    while (length > 0)
-    {
+    while (length > 0) {
         *dst++ = *src++;
         length -= 4;
     }
@@ -86,16 +76,14 @@ bool FlashCode::Read(uint32_t offset, uint32_t length, uint8_t* buffer, Result& 
     return true;
 }
 
-bool FlashCode::Erase(uint32_t offset, uint32_t length, flashcode::Result& result)
-{
+bool FlashCode::Erase(uint32_t offset, uint32_t length, flashcode::Result& result) {
     DEBUG_ENTRY();
     DEBUG_PRINTF("State=%d", static_cast<int>(s_state));
 
     result = Result::kOk;
 
-    switch (s_state)
-    {
-        case State::IDLE:
+    switch (s_state) {
+        case State::kIdle:
             s_page = offset + FLASH_BASE;
             s_length = length;
             fmc_unlock();
@@ -104,15 +92,13 @@ bool FlashCode::Erase(uint32_t offset, uint32_t length, flashcode::Result& resul
             return false;
             break;
         case State::ERASE_BUSY:
-            if (SET == fmc_flag_get(FMC_FLAG_BUSY))
-            {
+            if (SET == fmc_flag_get(FMC_FLAG_BUSY)) {
                 DEBUG_EXIT();
                 return false;
             }
 
-            if (s_length == 0)
-            {
-                s_state = State::IDLE;
+            if (s_length == 0) {
+                s_state = State::kIdle;
                 fmc_lock();
                 DEBUG_EXIT();
                 return true;
@@ -123,8 +109,7 @@ bool FlashCode::Erase(uint32_t offset, uint32_t length, flashcode::Result& resul
             return false;
             break;
         case State::ERASE_PROGAM:
-            if (s_length > 0)
-            {
+            if (s_length > 0) {
                 DEBUG_PRINTF("s_nPage=%p", s_page);
 
                 fmc_sector_erase(s_page);
@@ -148,20 +133,15 @@ bool FlashCode::Erase(uint32_t offset, uint32_t length, flashcode::Result& resul
     return true;
 }
 
-bool FlashCode::Write(uint32_t offset, uint32_t length, const uint8_t* buffer, flashcode::Result& result)
-{
-    if ((s_state == State::WRITE_PROGRAM) || (s_state == State::WRITE_BUSY))
-    {
-    }
-    else
-    {
+bool FlashCode::Write(uint32_t offset, uint32_t length, const uint8_t* buffer, flashcode::Result& result) {
+    if ((s_state == State::WRITE_PROGRAM) || (s_state == State::WRITE_BUSY)) {
+    } else {
         DEBUG_ENTRY();
     }
     result = Result::kOk;
 
-    switch (s_state)
-    {
-        case State::IDLE:
+    switch (s_state) {
+        case State::kIdle:
             DEBUG_PUTS("State::IDLE");
             s_address = offset + FLASH_BASE;
             s_data = const_cast<uint32_t*>(reinterpret_cast<const uint32_t*>(buffer));
@@ -172,23 +152,18 @@ bool FlashCode::Write(uint32_t offset, uint32_t length, const uint8_t* buffer, f
             return false;
             break;
         case State::WRITE_BUSY:
-            if (SET == fmc_flag_get(FMC_FLAG_BUSY))
-            {
+            if (SET == fmc_flag_get(FMC_FLAG_BUSY)) {
                 DEBUG_EXIT();
                 return false;
             }
 
-            if (s_length == 0)
-            {
+            if (s_length == 0) {
                 fmc_lock();
-                s_state = State::IDLE;
+                s_state = State::kIdle;
 
-                if (memcmp(reinterpret_cast<void*>(offset + FLASH_BASE), buffer, length) == 0)
-                {
+                if (memcmp(reinterpret_cast<void*>(offset + FLASH_BASE), buffer, length) == 0) {
                     DEBUG_PUTS("memcmp OK");
-                }
-                else
-                {
+                } else {
                     DEBUG_PUTS("memcmp failed");
                 }
 
@@ -200,10 +175,8 @@ bool FlashCode::Write(uint32_t offset, uint32_t length, const uint8_t* buffer, f
             return false;
             break;
         case State::WRITE_PROGRAM:
-            if (s_length >= 4)
-            {
-                if (FMC_READY == fmc_ready_wait(0xFF))
-                {
+            if (s_length >= 4) {
+                if (FMC_READY == fmc_ready_wait(0xFF)) {
                     /* set the PG bit to start program */
                     FMC_CTL |= FMC_CTL_PG;
                     __ISB();
@@ -217,9 +190,7 @@ bool FlashCode::Write(uint32_t offset, uint32_t length, const uint8_t* buffer, f
                     s_address += 4;
                     s_length -= 4;
                 }
-            }
-            else if (s_length > 0)
-            {
+            } else if (s_length > 0) {
                 DEBUG_PUTS("Error!");
             }
             s_state = State::WRITE_BUSY;
